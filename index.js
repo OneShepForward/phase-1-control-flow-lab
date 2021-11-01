584dd0fc3698e8c3a250119a549b2039e368b342
Nicholas Shephard <shephardn@gmail.com>
2021-11-01 11:51:28 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+let rideLength;
+let checkCity;
+
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength <= 400) {
+    return 'This one is on me!';
+  } else if (rideLength > 2000 && rideLength <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideLength > 2500) {
+    return 'No can do.';
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(checkCity){
   // Write your code here!
+  //Not sure why this ternary doesn't work...
+  // (checkCity === "NYC") ? 'Ok, sounds good.' : 'No go.';
+  
+  if (checkCity === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+        return 'Bye.';
+  }
 }
